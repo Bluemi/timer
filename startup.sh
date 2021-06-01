07a7b93b16b45996bb1d8054077ae419c94c793a
Bluemi <bruno.schilling@protonmail.ch>
2021-06-01 17:50:26 +0200

    added startup.sh

---
+++ startup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# You could copy this into your .profile
+/home/alok/.virtualenvs/timer/bin/python /home/alok/.local/bin/timerd > /home/alok/.local/etc/timerd/timerd.log 2> /home/alok/.local/etc/timerd/timerd.err &
